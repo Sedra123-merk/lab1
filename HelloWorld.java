e5daf1eedc4808cb377f768a3482c92127b9fb3f
sidra <sedra.merkbawi123@gmail.com>
2025-02-10 14:46:45 -0800

    Initial sharing of project

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+
+/**
+ * this is my first program in CISC190. It writes out Hello World and my name
+ *
+ * @author Sidra Merekbaoui , smerekbaoui@student.sdccd.edu
+ * @version (a version number or a date)
+ * @version v1.0 
+ * since 2/4/2025
+ */
+public class HelloWorld
+{
+    public static void main (String[]args) {
+        System.out.println("Hello World!");
+        System.out.println("Sidra Merekbaoui");
+   }
+}
